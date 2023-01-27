474a1933bac8523c2f896bc37d12245c463735ad
maks362880 <48396251+maks362880@users.noreply.github.com>
2023-01-27 14:07:54 +0300

    -- Fixed the logic public methods of Main class. Methods have been renamed in connection with the new logic of work. -- Fixed unit tests

--- com/gridnine/testing/main/Main.java
+++ com/gridnine/testing/main/Main.java
@@ -6,9 +6,7 @@ import com.gridnine.testing.main.model.Segment;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -18,24 +16,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
                 Выберите вариант:
-                1.Показать вылеты до текущего момента времени.\s
-                2.Показать имеются ли сегменты с датой прилёта раньше даты вылета.
-                3.Показать если общее время, проведённое на земле превышает два часа.""");
+                1.Исключить вылеты до текущего момента времени.\s
+                2.Исключить имеются ли сегменты с датой прилёта раньше даты вылета.
+                3.Исключить если общее время, проведённое на земле превышает два часа.""");
         int sortOption = scanner.nextInt();
         switch (sortOption) {
-            case (1) -> System.out.println(getSegmentsUpToCurrentTime(flightList));
-            case (2) -> System.out.println(arrivalDateBeforeDepartureDate(flightList));
-            case (3) -> System.out.println(totalTimeOnTheGroundExceedsTwoHours(flightList));
+            case (1) -> System.out.println(excludeSegmentsUpToCurrentTime(flightList));
+            case (2) -> System.out.println(excludeArrivalDateBeforeDepartureDate(flightList));
+            case (3) -> System.out.println(excludeTotalTimeOnTheGroundExceedsTwoHours(flightList));
             default -> System.out.println("Выбран неверный параметр");
         }
     }
 
-    public static List<Flight> totalTimeOnTheGroundExceedsTwoHours(List<Flight> flightList) {
+    public static List<Flight> excludeTotalTimeOnTheGroundExceedsTwoHours(List<Flight> flightList) {
         if (flightList.isEmpty()) {
             throw new IllegalArgumentException("");
         }
-        List<Flight> resultList = new ArrayList<>();
-        for (Flight flight : flightList) {
+        List<Flight> resultFlightList = new ArrayList<>(flightList);
+        Iterator<Flight> iterator = resultFlightList.iterator();
+        while (iterator.hasNext()) {
+            Flight flight = iterator.next();
             List<Segment> segments = flight.getSegments();
             long dateDiffInMinutes = 0;
             for (int i = 1; i < segments.size(); i++) {
@@ -44,35 +44,35 @@ public class Main {
                                 segments.get(i - 1).getArrivalDate(), segments.get(i).getDepartureDate());
             }
             if (dateDiffInMinutes > 120) {
-                resultList.add(flight);
+                iterator.remove();
             }
         }
-        return resultList;
+        return resultFlightList;
     }
 
-    public static List<Segment> arrivalDateBeforeDepartureDate(List<Flight> flightList) {
+    public static List<Flight> excludeArrivalDateBeforeDepartureDate(List<Flight> flightList) {
         if (flightList.isEmpty()) {
             throw new IllegalArgumentException("");
         }
-        List<Segment> resultList = new ArrayList<>();
-        flightList.stream()
-                .flatMap(segment -> segment.getSegments().stream())
-                .filter(element -> element.getDepartureDate().isAfter(element.getArrivalDate()))
-                .forEach(resultList::add);
-        return resultList;
+        List<Flight> resultFlightList = new ArrayList<>(flightList);
+        resultFlightList
+                .removeIf(flight -> flight.getSegments()
+                .stream()
+                .anyMatch(element -> element.getDepartureDate().isAfter(element.getArrivalDate())));
+        return resultFlightList;
     }
 
-    public static List<Segment> getSegmentsUpToCurrentTime(List<Flight> flightList) {
+    public static List<Flight> excludeSegmentsUpToCurrentTime(List<Flight> flightList) {
         if (flightList.isEmpty()) {
             throw new IllegalArgumentException("");
         }
-        List<Segment> resultList = new ArrayList<>();
+        List<Flight> resultFlightList = new ArrayList<>(flightList);
         LocalDateTime localDateTime = LocalDateTime.now();
-        flightList.stream()
-                .flatMap(segment -> segment.getSegments().stream())
-                .filter(element -> element.getDepartureDate().isBefore(localDateTime))
-                .forEach(resultList::add);
-        return resultList;
+        resultFlightList
+                .removeIf(flight -> flight.getSegments()
+                        .stream()
+                .anyMatch(element -> element.getDepartureDate().isBefore(localDateTime)));
+        return resultFlightList;
     }
 
 }
--- com/gridnine/testing/test/MainTest.java
+++ com/gridnine/testing/test/MainTest.java
@@ -1,8 +1,6 @@
 package com.gridnine.testing.test;
 
-import com.gridnine.testing.main.Main;
 import com.gridnine.testing.main.model.Flight;
-import com.gridnine.testing.main.model.Segment;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -19,26 +17,26 @@ class MainTest {
     class PositiveTests {
 
         @Test
-        void totalTimeOnTheGroundExceedsTwoHours_success() {
-            List<Segment> segments = generateSegments();
+        void excludeTimeOnTheGroundExceedsTwoHours_success() {
             List<Flight> flightList = generateListFlight();
-            var flightsMore2Hours = Main.totalTimeOnTheGroundExceedsTwoHours(flightList);
+            List<Flight> flightsMore2Hours = excludeTotalTimeOnTheGroundExceedsTwoHours(flightList);
             assertEquals(1, flightsMore2Hours.size());
-            assertEquals(flightsMore2Hours.get(0), flightList.get(0));
+            assertEquals(flightsMore2Hours.get(0), flightList.get(1));
+
         }
 
         @Test
         void arrivalDateBeforeDepartureDate_success() {
-            List<Segment> segments = generateSegments2();
             List<Flight> flightList = generateListFlight();
-            assertEquals(segments.get(0),
-                    arrivalDateBeforeDepartureDate(flightList).get(0));
+            assertEquals(2, flightList.size());
+            assertEquals(flightList.get(0),
+                    excludeArrivalDateBeforeDepartureDate(flightList).get(0));
         }
 
         @Test
         void getSegmentsUpToCurrentTime_success() {
             List<Flight> flightList = generateListFlight();
-            assertEquals(flightList.get(1).getSegments().get(0), getSegmentsUpToCurrentTime(flightList).get(0));
+            assertEquals(flightList.get(0), excludeSegmentsUpToCurrentTime(flightList).get(0));
         }
 
     }
@@ -49,19 +47,19 @@ class MainTest {
         @Test
         void When_ListOfFlightISEmpty_totalTimeOnTheGroundExceedsTwoHours_Exception() {
             List<Flight> unExpectedAnswer = new ArrayList<>();
-            assertThrows(IllegalArgumentException.class, () -> totalTimeOnTheGroundExceedsTwoHours(unExpectedAnswer));
+            assertThrows(IllegalArgumentException.class, () -> excludeTotalTimeOnTheGroundExceedsTwoHours(unExpectedAnswer));
         }
 
         @Test
         void When_ListOfFlightISEmpty_arrivalDateBeforeDepartureDate_Exception() {
             List<Flight> unExpectedAnswer = new ArrayList<>();
-            assertThrows(IllegalArgumentException.class, () -> arrivalDateBeforeDepartureDate(unExpectedAnswer));
+            assertThrows(IllegalArgumentException.class, () -> excludeArrivalDateBeforeDepartureDate(unExpectedAnswer));
         }
 
         @Test
         void When_ListOfFlightISEmpty_getSegmentsUpToCurrentTime_Exception() {
             List<Flight> unExpectedAnswer = new ArrayList<>();
-            assertThrows(IllegalArgumentException.class, () -> getSegmentsUpToCurrentTime(unExpectedAnswer));
+            assertThrows(IllegalArgumentException.class, () -> excludeSegmentsUpToCurrentTime(unExpectedAnswer));
         }
     }
 
